06812084f9e8c9d2e54dd7526c109926b94fd8fa
Alexey Donskoy <aleksey8996@mail.ru>
2024-04-23 16:08:27 +0500

    add class contact

---
+++ scricpt.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, last_name, first_name, middle_name, phone_number):
+        # Класс Contact для создания новго контакта
+        self.last_name = last_name
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.phone_number = phone_number
+
+        
